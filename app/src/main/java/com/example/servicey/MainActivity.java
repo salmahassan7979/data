bfd3faa8650dd710f6f85936c6d1889a7080c461
ghadamohamedfahmy <48032855+ghadamohamedfahmy@users.noreply.github.com>
2019-04-18 14:52:26 +0200

    home pag flip

--- app/src/main/java/com/example/servicey/MainActivity.java
+++ app/src/main/java/com/example/servicey/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.servicey;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,page2_loginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
